c0c1452195935867866ef15c25a77ea0ac5633d8
Chuang Wang <chuangw.sde@gmail.com>
2021-02-25 17:06:13 -0800

    add docstring

--- pypkgs_chuangw/pypkgs_chuangw.py
+++ pypkgs_chuangw/pypkgs_chuangw.py
@@ -2,19 +2,30 @@ import pandas as pd
 
 
 def catbind(a, b):
-    """ concatenate two Pandas categoricals
+    """
+    Concatenates two pandas categoricals.
 
     Parameters
     ----------
     a : pandas.core.arrays.categorical.Categorical
-        the categorical to concat
+      A pandas categorical.
     b : pandas.core.arrays.categorical.Categorical
-        the categorical to concat
+      A pandas categorical that you wish to concatenate to a.
 
     Returns
     -------
     pandas.core.arrays.categorical.Categorical
-        the concatenation (joining) of two Pandas categoricals
+      The new concatenated pandas categorical.
+
+    Examples
+    --------
+    >>> from pypkgs import pypkgs
+    >>> a = pd.Categorical(["character", "hits", "your", "eyeballs"])
+    >>> b = pd.Categorical(["but", "integer", "where it", "counts"])
+    >>> pypkgs.catbind(a, b)
+    [character, hits, your, eyeballs, but, integer, where it, counts]
+    Categories (8, object): [but, character, counts,
+    eyeballs, hits, integer, where it, your]
     """
     concatenated = pd.concat([pd.Series(a.astype("str")), pd.Series(b.astype("str"))])
     return pd.Categorical(concatenated)
